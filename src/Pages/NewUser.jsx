c34a19eef4eaef785ecef126f6377c3d7adc92ba
nrocpop <tarobinsontoast@gmail.com>
2022-09-12 16:10:02 -0400

    Updated New User Page

--- src/Pages/NewUser.jsx
+++ src/Pages/NewUser.jsx
@@ -3,51 +3,51 @@ import styles from "./Styles/NewUser.module.css"
 
 function  NewUser() {return(
 
-    <div class="holder-box">
-        <div class="title">Create New User</div>
-            <div class="components">
+    <div class={styles.holderBox}>
+        <div class={styles.title}>Create New User</div>
+            <div className = {styles.components}>
             
                 <form action="#"/>
-                    <div class="user-information">
-                        <div class="styles.input-box">
-                         <span class="styles.information">First Name</span>
+                    <div class={styles.userInformation}>
+                        <div class={styles.inputBox}>
+                         <span class={styles.information}>First Name</span>
                             <input type="text" placeholder="Enter your first name" required/>
                         </div>
-                        <div class="styles.input-box">
-                        <span class="styles.information">Last Name</span>
+                        <div class={styles.inputBox}>
+                        <span class={styles.information}>Last Name</span>
                         <input type="styles.text" placeholder="Enter your last name" required/>
                         </div>
-                        <div class="styles.input-box">
-                        <span class="styles.information">Date of Birth</span>
+                        <div class={styles.inputBox}>
+                        <span class={styles.information}>Date of Birth</span>
                         <input type="date" required/>
                         </div>
-                        <div class="styles.input-box">
-                        <span class="styles.information">Phone Number</span>
+                        <div class={styles.inputBox}>
+                        <span class={styles.information}>Phone Number</span>
                         <input type="tel" placeholder="(___) ___-_____" required/>
                         </div>
-                        <div class="styles.input-box">
-                        <span class="styles.information">Street Address</span>
+                        <div class={styles.inputBox}>
+                        <span class={styles.information}>Street Address</span>
                         <input type="text" placeholder="Enter your address" required/>
                         </div>
-                        <div class="styles.input-box">
-                        <span class="styles.information">City</span>
+                        <div class={styles.inputBox}>
+                        <span class={styles.information}>City</span>
                         <input type="text" placeholder="Enter city" required/>
                         </div>
-                        <div class="styles.input-box">
-                        <span class="styles.information">State</span>
+                        <div class={styles.inputBox}>
+                        <span class={styles.information}>State</span>
                         <input type="text" placeholder="Enter state" required/>
                         </div>
-                        <div class="styles.input-box">
-                        <span class="styles.information">ZIP/Postal Code</span>
+                        <div class={styles.inputBox}>
+                        <span class={styles.information}>ZIP/Postal Code</span>
                         <input type="text" placeholder="Enter ZIP/Postal code" required/>
                         </div>
                     </div>
-                <div class="styles.account-type">
-                    <input type="radio-button" name="account-type" id="radio-one"/>
-                    <input type="radio-button" name="account-type" id="radio-two"/>
-                    <input type="radio-button" name="account-type" id="radio-three"/>
+                <div class={styles.accountType}>
+                    <input type="radio" name="account-type" id="radio-one"/>
+                    <input type="radio" name="account-type" id="radio-two"/>
+                    <input type="radio" name="account-type" id="radio-three"/>
                     <span class="styles.account-title">Account Type</span>
-                    <div class="styles.category">
+                    <div class={styles.category}>
                         <label for="radio-one">
                             <span class="styles.radio-one"></span>
                             <span class="styles.account-type">Administrator</span>
@@ -62,7 +62,7 @@ function  NewUser() {return(
                     </div>
                 </div>
                 <div class="styles.button">
-                    <input type="register" value="Register"/>
+                    <input type="button" value="Register"/>
                 </div>
             </div>
             
